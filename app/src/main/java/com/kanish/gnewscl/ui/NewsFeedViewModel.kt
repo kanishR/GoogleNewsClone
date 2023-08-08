a56385f0ad03323fa10bd2749280272e61b796d4
kanish <kanro@gmail.com>
2023-08-08 16:37:06 +0530

    * updated states //TODO *Error parsing *UI

--- app/src/main/java/com/kanish/gnewscl/ui/NewsFeedViewModel.kt
+++ app/src/main/java/com/kanish/gnewscl/ui/NewsFeedViewModel.kt
@@ -1,19 +1,25 @@
 package com.kanish.gnewscl.ui
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.kanish.gnewscl.data.repository.NewsFeedRepository
+import com.kanish.gnewscl.domain.NewsFeedScreenState
 import kotlinx.coroutines.launch
 
 class NewsFeedViewModel(private  val repository: NewsFeedRepository,
                         application: Application):AndroidViewModel(application) {
+    val mNewsFeedState: MutableLiveData<NewsFeedScreenState> = MutableLiveData()
 
     fun fetchNewsFeed(){
         viewModelScope.launch {
+            setViewState(NewsFeedScreenState.Loading)
            val data = repository.fetchNewsFeed("entertainment")
-            Log.d("kanish","data >>"+ data)
+            setViewState(NewsFeedScreenState.NewsFeedAvailable(data))
         }
+    }
+    fun setViewState(state:NewsFeedScreenState){
+        mNewsFeedState.postValue(state)
     }
 }
